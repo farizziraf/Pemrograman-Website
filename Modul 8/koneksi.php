27b96b77a5ee1caca32d6c63111d3b9d7bb0c1ba
farizziraf <efanaha1324@gmail.com>
2023-05-08 10:59:03 +0700

    pemweb

--- Modul 8/koneksi.php
+++ Modul 8/koneksi.php
@@ -2,8 +2,8 @@
 // konfigurasi database
 $host = "localhost";
 $user = "root";
-$password - "";
-$database - "latihan";
+$password = "";
+$database = "latihan";
 //perintah php untuk akses ke database
 $koneksi = mysqli_connect($host, $user, $password, $database);
 ?>
