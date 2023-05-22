75cdd1d31964ec8588c045ba64727452e371aab9
farizziraf <efanaha1324@gmail.com>
2023-05-22 11:19:52 +0700

    pemweb

--- Modul 10/grafikpie.php
+++ Modul 10/grafikpie.php
@@ -1,14 +1,3 @@
-<?php
-include('koneksi.php');
-$produk = mysqli_query($koneksi, "select * from tb_barang");
-while ($row = mysqli_fetch_array($produk)) {
-    $nama_produk[] = $row['barang'];
-    $query = mysqli_query($koneksi, "select sum(jumlah) as jumlah from 
-tb_penjualan where id_barang='" . $row['id_barang'] . "'");
-    $row = $query->fetch_array();
-    $jumlah_produk[] = $row['jumlah'];
-}
-?>
 <!doctype html>
 <html>
 
@@ -22,12 +11,22 @@ tb_penjualan where id_barang='" . $row['id_barang'] . "'");
         <canvas id="chart-area"></canvas>
     </div>
     <script>
+        <?php
+        include('koneksi.php');
+        $produk = mysqli_query($koneksi, "SELECT * FROM tb_barang");
+        while ($row = mysqli_fetch_array($produk)) {
+            $nama_produk[] = $row['barang'];
+            $query = mysqli_query($koneksi, "SELECT SUM(jumlah) as jumlah FROM tb_penjualan WHERE id_barang='" . $row['id_barang'] . "'");
+            $row = $query->fetch_array();
+            $jumlah_produk[] = $row['jumlah'];
+        }
+        ?>
+
         var config = {
             type: 'pie',
             data: {
                 datasets: [{
-                    data: <?php echo json_encode($jumlah_produk);
-                    ?>,
+                    data: <?php echo json_encode($jumlah_produk); ?>,
                     backgroundColor: [
                         'rgba(255, 99, 132, 0.2)',
                         'rgba(54, 162, 235, 0.2)',
@@ -48,44 +47,44 @@ tb_penjualan where id_barang='" . $row['id_barang'] . "'");
                 responsive: true
             }
         };
+
         window.onload = function () {
-            var ctx = document.getElementById('chartarea').getContext('2d');
+            var ctx = document.getElementById('chart-area').getContext('2d');
             window.myPie = new Chart(ctx, config);
         };
-        document.getElementById('randomizeData').addEventListener('click',
-            function () {
-                config.data.datasets.forEach(function (dataset) {
-                    dataset.data = dataset.data.map(function () {
-                        return randomScalingFactor();
-                    });
+
+        document.getElementById('randomizeData').addEventListener('click', function () {
+            config.data.datasets.forEach(function (dataset) {
+                dataset.data = dataset.data.map(function () {
+                    return randomScalingFactor();
                 });
-                window.myPie.update();
             });
+            window.myPie.update();
+        });
+
         var colorNames = Object.keys(window.chartColors);
-        document.getElementById('addDataset').addEventListener('click',
-            function () {
-                var newDataset = {
-                    backgroundColor: [],
-                    data: [],
-                    label: 'New dataset ' +
-                        config.data.datasets.length,
-                };
-                for (var index = 0; index < config.data.labels.length;
-                    ++index) {
-                    newDataset.data.push(randomScalingFactor());
-                    var colorName = colorNames[index %
-                        colorNames.length];
-                    var newColor = window.chartColors[colorName];
-                    newDataset.backgroundColor.push(newColor);
-                }
-                config.data.datasets.push(newDataset);
-                window.myPie.update();
-            });
-        document.getElementById('removeDataset').addEventListener('click',
-            function () {
-                config.data.datasets.splice(0, 1);
-                window.myPie.update();
-            });
+        document.getElementById('addDataset').addEventListener('click', function () {
+            var newDataset = {
+                backgroundColor: [],
+                data: [],
+                label: 'New dataset ' + config.data.datasets.length,
+            };
+
+            for (var index = 0; index < config.data.labels.length; ++index) {
+                newDataset.data.push(randomScalingFactor());
+                var colorName = colorNames[index % colorNames.length];
+                var newColor = window.chartColors[colorName];
+                newDataset.backgroundColor.push(newColor);
+            }
+
+            config.data.datasets.push(newDataset);
+            window.myPie.update();
+        });
+
+        document.getElementById('removeDataset').addEventListener('click', function () {
+            config.data.datasets.splice(0, 1);
+            window.myPie.update();
+        });
     </script>
 </body>
 
